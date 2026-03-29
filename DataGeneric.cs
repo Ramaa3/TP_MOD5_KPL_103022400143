664a40f6fed1f694941054b66d9354c57ccc5a89
Ramaa3 <ramnasyaifaarliansyah@student.telkomuniversity.ac.id>
2026-03-29 23:20:54 +0700

    menambahkan class DataGeneric

---
+++ DataGeneric.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL5_103022401043
+{
+    internal class DataGeneric<T>{
+
+            // Property generic
+            public T Data{ get; set; }
+
+            // Constructor
+            public DataGeneric(T data)
+            {
+                Data = data;
+            }
+
+            // Method untuk print data
+            public void PrintData()
+            {
+                Console.WriteLine("Data yang tersimpan adalah: " + Data);
+            }
+        
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MODUL5_103022401043;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Isi dengan NIM kamu (contoh)
+        DataGeneric<string> data = new DataGeneric<string>("103022401043");
+
+        data.PrintData();
+
+        Console.ReadLine();
+    }
+}
