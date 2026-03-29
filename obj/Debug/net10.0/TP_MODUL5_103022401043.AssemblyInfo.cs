622d60f3d97f07d22fd963ac70c2252e6ce20c7f
Ramaa3 <ramnasyaifaarliansyah@student.telkomuniversity.ac.id>
2026-03-29 23:28:30 +0700

    resolve conflict

--- obj/Debug/net10.0/TP_MODUL5_103022401043.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL5_103022401043.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL5_103022401043")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664a40f6fed1f694941054b66d9354c57ccc5a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL5_103022401043")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL5_103022401043")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
